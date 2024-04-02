daf2513343629b114bf69024ffb03faf645017e2
DiegoValdivieso22 <159164145+DiegoValdivieso22@users.noreply.github.com>
2024-04-02 21:18:16 +0000

    Edit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
 function doMathClicked() {
-  // this function does basic math
+  // this function does basic math: Mph to Kph
   let Mph = document.getElementById("Mph").value;
-  let Kph = Mph * 1.60;
   let answer = Mph * 1.60;
-  
-  document.getElementById("Kph").innerHTML = Mph + " * 1.60 = " + answer.toFixed(2);
 
+  // Make sure not negative
+  if (answer < 0) {
+    answer = 0;
+    document.getElementById("Kph").innerHTML = "Invalid input. Ensure value is a positive number.";
+  }
+  else {
+    document.getElementById("Kph").innerHTML = "Kph = " + answer.toFixed(2);
+  }
+}
+function kphToMetersPH() {
+  let kph = document.getElementById("Kph").value;
+  let calculation = kph * 1000;
+
+  // Make sure not negative
+  if (calculation <= 0) {
+    calculation = 0;
+    document.getElementById("metersperhour").innerHTML = "Invalid input. Ensure value is a positive number.";
+  }
+  else {
+    document.getElementById("metersperhour").innerHTML = "Meters per hour is = " + calculation.toFixed(2);
+
+  }
 }
